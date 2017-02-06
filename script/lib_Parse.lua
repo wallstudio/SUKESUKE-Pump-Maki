982a8e82ba010b3ce3e4d1b1883c9ca855448d2e
wallstudio <com.haru.31+wallstudio@gmail.com>
2017-02-06 20:04:12 +0900

    タンクの解析完成

--- script/lib_Parse.lua
+++ script/lib_Parse.lua
@@ -39,12 +39,44 @@ function BetweenPixcel(start, stop)
     Debug.Log("#sample: " .. #rtn);
     for i=1,#rtn do
         if 50 > math.abs(rtn[i][longEdge] - start[longEdge]) then
-            Debug.Point(rtn[i][1]-3, rtn[i][2], 1, Color.SKYBLUE, 1);
+            --Debug.Point(rtn[i][1]-3, rtn[i][2], 1, Color.SKYBLUE, 1);
         else
-            Debug.Point(rtn[i][1]+3, rtn[i][2], 1, Color.BLUE, 1);
+            --Debug.Point(rtn[i][1]+3, rtn[i][2], 1, Color.BLUE, 1);
         end
     end
-    Debug.Point(rtn[1][1]+3, rtn[1][2], 2, Color.BLACK, 1);
+    Debug.Point(rtn[1][1], rtn[1][2], 24, Color.BLACK, 0.3);
     Debug.Log("#sample: " .. #rtn);
     return rtn;
+end
+function ParseColor(samplePx, ruleFunc)
+    local rateArry = {};
+    for i=1,#samplePx do
+        obj.pixeloption("type","rgb");
+        local r, g, b = obj.getpixel(samplePx[i][1], samplePx[i][2], "rgb");
+        rateArry [i] = ruleFunc(r, g, b);
+        DebugShow(samplePx[i][1], samplePx[i][2], ruleFunc);
+    end
+    local counter = 0;
+    for i=1,#rateArry do
+        if rateArry[i] then
+            counter = counter + 1;
+        end
+    end
+    return counter/#rateArry;
+end
+function DebugShow(x, y, ruleFunc)
+    local OFFSET = 100;
+    local RANGE = 40;
+    if EDIT then
+        for i=1,RANGE do
+            obj.pixeloption("type","rgb");
+            local r, g, b = obj.getpixel(x + i - RANGE/2, y, "rgb");
+            obj.pixeloption("type","col");
+            if(ruleFunc(r, g, b)) then
+                Editor.Point(x + i - RANGE/2 - OFFSET, y, 1, Color.WHITE, 1);
+            else
+                Editor.Point(x + i - RANGE/2 - OFFSET, y, 1, Color.BLACK, 1);
+            end
+        end
+    end
 end
---
+++ script/lib_Sunshine.lua
@@ -0,0 +1,21 @@
+module("Sunshine",package.seeall);
+TANK_WEIGHT = {0, 1.1, 0.5};
+TANK_BORDER = 0.5;
+HP_WEIGHT = {0.1, 1.6, 0.2};
+HP_BORDER = 1.0;
+function Parse(r, g, b, weight, border)
+    local l = ( 0.298912 * weight[1] * r + 0.586611 * weight[2] * g + 0.114478 * weight[3] *  b );
+    --local l = ( 0.298912 * obj.track0 * r + 0.586611 * obj.track1 * g + 0.114478 * obj.track2 *  b );
+    if l < 255 * border then
+        return false;
+    else
+        return true;
+    end
+    Debug.Log("EXCEPTION in Parse");
+end
+function TankParse(r, g, b)
+    return Parse(r, g, b, TANK_WEIGHT, TANK_BORDER);
+end
+function HpParse(r, g, b)
+    return Parse(r, g, b, HP_WEIGHT, HP_BORDER);
+end
